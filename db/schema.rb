1affdbfda9031fc1d4de8565b745765de5ee8745
NISHIMURA, Sota <melmaga.fallere246@gmail.com>
2018-11-06 18:41:01 +0900

    add goods

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_30_113740) do
+ActiveRecord::Schema.define(version: 2018_11_06_093143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "goods", force: :cascade do |t|
     t.string "name"
     t.integer "price"
     t.datetime "created_at", null: false
